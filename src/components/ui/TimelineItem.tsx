e6a39d243e6a5484a35326aca80d8a57b23b0413
Josip Anic <josipanic1985@gmail.com>
2023-11-24 15:24:58 +0100

    create timeline example

--- src/components/ui/TimelineItem.tsx
+++ src/components/ui/TimelineItem.tsx
@@ -1,15 +1,17 @@
 import {TagGroup} from "@/components/ui/TagGroup";
 
 interface ExperienceItemProps {
+  from: string
+  to: string
   title: string
   description: string
   tags: string[]
 }
 export const TimelineItem = ({ from, to, title, description, tags }: ExperienceItemProps) => {
   return (
-    <article className="grid grid-cols-8 gap-8 items-start">
-      <div className="relative pt-5 col-span-2 after:absolute after:content-[''] after:block after:rounded-full after:bg-secondary after:w-4 after:h-4 after:top-7 after:-right-2">
-        <h3 className="text-xl font-bold">
+    <article className="grid grid-cols-8 gap-12 items-start">
+      <div className="relative pt-5 col-span-2 after:absolute after:content-[''] after:block after:rounded-full after:bg-secondary after:w-4 after:h-4 after:top-7 after:-right-8">
+        <h3 className="text-xl font-bold text-right">
           {from} - {to}
         </h3>
       </div>
