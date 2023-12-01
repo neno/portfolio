b56444c8b69effbca15301fe2a43bf9814e695ea
Nenad Brcic <neno@neno.ch>
2023-12-01 11:33:53 +0100

    Add sectionTag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <header>
-          <Section slug='header' />
-        </header>
+        <Section slug='header' />
         <main>{children}</main>
       </body>
     </html>
